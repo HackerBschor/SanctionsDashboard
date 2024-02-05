97a69b706f3fcde94eb5596f9614275ccd332f55
Nicolas Bschor <nico.bschor@gmail.com>
2024-02-05 18:55:58 +0100

    app in wrapper

--- sanctions_dashboard/dashboard.py
+++ sanctions_dashboard/dashboard.py
@@ -24,7 +24,8 @@ with open("../data/schemas.txt", "r") as f:
 
 
 with open("../data/industries.txt", "r") as f:
-    industries = [{"value": v, "label": " ".join([x.capitalize() for x in v.replace("/", " / ").split(" ")])} for v in f.read().split("\n")]
+    industries = [{"value": v, "label": " ".join([x.capitalize() for x in v.replace("/", " / ").split(" ")])} for v in
+                  f.read().split("\n")]
 
 entity_search_header: list = [
     {'name': i, 'id': i, 'deletable': True} for i in
@@ -57,151 +58,155 @@ tbl_inter_company_header: list = [
     {'name': "Source Country", 'id': "source", 'deletable': True}
 ]
 
-###################################################################
-# Dash Layout
-###################################################################
 
-app: Dash = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+def create_app():
+    ###################################################################
+    # Dash Layout
+    ###################################################################
 
-app.layout = html.Div([
-    dbc.Container([
-        # html.H1("OpenSanctions Dashboard"),
-        html.H4(""),
-        dcc.Tabs(value='Sanctions by Country', children=[
-            # TAB 1
-            dcc.Tab(label='Sanctions by Country', value='Sanctions by Country', children=[
-                html.H4("Filter"),
-                dbc.Row([
-                    dbc.Col(dmc.Select(id='sbc-filter-mode', data=["Sanctions towards", "Sanctions from"], value="Sanctions towards"), width=6, lg=2),
-                    dbc.Col(dmc.Select(id='sbc-filter-country', data=target_countries, placeholder="Country", searchable=True), width=6, lg=3),
-                    dbc.Col(dmc.DatePicker(id="sbc-filter-start-date", placeholder="Start Date", minDate=date(201, 5, 21)), width=6, lg=2),
-                    dbc.Col(dmc.DatePicker(id="sbc-filter-end-date", placeholder="End Date", minDate=date(201, 5, 21)), width=6, lg=2),
-                    dbc.Col(dbc.Button(id="sbc-btn-export", children="Excel Export", color="primary", n_clicks=0), width=12, lg=2)
+    app: Dash = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+    app.layout = html.Div([
+        dbc.Container([
+            # html.H1("OpenSanctions Dashboard"),
+            html.H4(""),
+            dcc.Tabs(value='Sanctions by Country', children=[
+                # TAB 1
+                dcc.Tab(label='Sanctions by Country', value='Sanctions by Country', children=[
+                    html.H4("Filter"),
+                    dbc.Row([
+                        dbc.Col(dmc.Select(id='sbc-filter-mode', data=["Sanctions towards", "Sanctions from"], value="Sanctions towards"), width=6, lg=2),
+                        dbc.Col(dmc.Select(id='sbc-filter-country', data=target_countries, placeholder="Country", searchable=True), width=6, lg=3),
+                        dbc.Col(dmc.DatePicker(id="sbc-filter-start-date", placeholder="Start Date", minDate=date(201, 5, 21)), width=6, lg=2),
+                        dbc.Col(dmc.DatePicker(id="sbc-filter-end-date", placeholder="End Date", minDate=date(201, 5, 21)), width=6, lg=2),
+                        dbc.Col(dbc.Button(id="sbc-btn-export", children="Excel Export", color="primary", n_clicks=0), width=12, lg=2)
+                    ]),
+                    html.Br(),
+                    dbc.Row([
+                        dbc.Col(dmc.Select(id='sbc-filter-schema', data=schemas, placeholder="Schema", searchable=True), width=6, lg=2),
+                        dbc.Col(dmc.Select(id='sbc-filter-industries', data=industries, placeholder="Industries", searchable=True), width=6, lg=3),
+                    ]),
+
+                    html.Br(),
+                    html.H4("Number of Entities by Country"),
+                    html.P("How many sanctions (entities) have been imposed on this countries or are imposed by this country"),
+                    dbc.Row(dcc.Graph(id="sbc-graph-sanctions-by-country")),
+                    html.H4("Number of Entries by Date"),
+                    html.P("On which date the entity occurred on one of the sanctions datasets"),
+                    dbc.Row(dcc.Graph(id="sbc-graph-sanctions-timeline")),
+
+                    dbc.Row([
+                        dbc.Col(html.H4("Number of Entries by Schema"), lg=6),
+                        dbc.Col(html.H4("Number of Entries by Industry"), lg=6),
+                    ]),
+
+                    dbc.Row([
+                        dbc.Col(html.P("Which type of entities (in or from a specific country) experience the most sanctions towards them"), lg=6),
+                        dbc.Col(html.P("Which company industries (in or from a specific country) experience the most sanctions towards them"), lg=6),
+                    ]),
+
+                    dbc.Row([
+                        dbc.Col([dcc.Graph(id="sbc-graph-sanctions-schemas")], lg=6),
+                        dbc.Col([dcc.Graph(id="sbc-graph-sanctions-industry")], lg=6),
+                    ]),
+
+                    html.Br(),
+                    html.H4("Results"),
+                    dbc.Row([dash_table.DataTable(
+                        id='sbc-tbl-results', data=[], columns=tbl_inter_company_header,
+                        style_cell={"whiteSpace": "pre-line"}, sort_action="native", sort_mode='multi',
+                        row_deletable=False, page_action='native', page_current=0, page_size=10
+                    )])
                 ]),
-                html.Br(),
-                dbc.Row([
-                    dbc.Col(dmc.Select(id='sbc-filter-schema', data=schemas, placeholder="Schema", searchable=True), width=6, lg=2),
-                    dbc.Col(dmc.Select(id='sbc-filter-industries', data=industries, placeholder="Industries", searchable=True), width=6, lg=3),
+
+                # TAB 2
+                dcc.Tab(label='Entities', value='entities', children=[
+                    html.H4("Search"),
+                    dbc.Row([
+                        dbc.Col(dmc.TextInput(id="entities-filter-query", type="text", placeholder="Query", debounce=True), width=12, lg=4),
+                        dbc.Col(dmc.Select(id='entities-filter-country', data=[""] + target_countries, placeholder="Country", searchable=True), width=6, lg=3),
+                        dbc.Col(dmc.Select(id='entities-filter-schema', data=schemas, placeholder="Schema", searchable=True), width=6, lg=2),
+                        dbc.Col(dbc.Button(id='entities-btn-search', children="Search", color="light", className="me-1", n_clicks=0), width=6, lg=1),
+                        dbc.Col(dbc.Button(id='entities-btn-export', children="Excel Export", color="primary", className="me-1", n_clicks=0), width=6, lg=2)
+                    ]),
+                    html.Br(),
+                    html.H4("Result"),
+                    dbc.Row([dash_table.DataTable(
+                        id='entities-tbl-results', columns=entity_search_header, data=[],
+                        style_cell={"whiteSpace": "pre-line"}, sort_action="native", sort_mode='multi',
+                        row_deletable=False, page_action='native', page_current=0, page_size=10
+                    )])
                 ]),
 
-                html.Br(),
-                html.H4("Number of Entities by Country"),
-                html.P("How many sanctions (entities) have been imposed on this countries or are imposed by this country"),
-                dbc.Row(dcc.Graph(id="sbc-graph-sanctions-by-country")),
-                html.H4("Number of Entries by Date"),
-                html.P("On which date the entity occurred on one of the sanctions datasets"),
-                dbc.Row(dcc.Graph(id="sbc-graph-sanctions-timeline")),
+                # TAB 3
+                dcc.Tab(label='Network Analysis', value='Network Analysis', children=[
+                    html.H4("Filter"),
+                    dbc.Row([
+                        dbc.Col(dmc.Select(id='network-filter-schema', data=schemas, placeholder="Schema", searchable=True), width=6, lg=2),
+                        dbc.Col(dmc.Select(id='network-filter-industry', data=industries, placeholder="Industries", searchable=True), width=6, lg=3),
+                        dbc.Col(dmc.DatePicker(id="network-filter-start-date", placeholder="Start Date", minDate=date(201, 5, 21)), width=6, lg=2),
+                        dbc.Col(dmc.DatePicker(id="network-filter-end-date", placeholder="End Date", minDate=date(201, 5, 21)), width=6, lg=2),
+                        dbc.Col(dbc.Button(id="network-btn-export", children="Excel Export", color="primary", className="me-1", n_clicks=0), width=4, lg=2),
+                    ]),
+                    html.Br(),
+                    dbc.Row([
+                        dbc.Col(dmc.MultiSelect(id="network-filter-countries", searchable=True, placeholder="Countries", data=all_countries), width=12, lg=9),
+                        dbc.Col(dbc.Button(id="network-btn-load", children="Load", color="light", n_clicks=0), width=4, lg=1),
+                    ]),
+                    html.Br(),
+                    html.H4("Who Sanctions Whom"),
+                    dcc.Graph(id="network-graph"),
 
-                dbc.Row([
-                    dbc.Col(html.H4("Number of Entries by Schema"), lg=6),
-                    dbc.Col(html.H4("Number of Entries by Industry"), lg=6),
-                ]),
-
-                dbc.Row([
-                    dbc.Col(html.P("Which type of entities (in or from a specific country) experience the most sanctions towards them"), lg=6),
-                    dbc.Col(html.P("Which company industries (in or from a specific country) experience the most sanctions towards them"), lg=6),
-                ]),
-
-                dbc.Row([
-                    dbc.Col([dcc.Graph(id="sbc-graph-sanctions-schemas")], lg=6),
-                    dbc.Col([dcc.Graph(id="sbc-graph-sanctions-industry")], lg=6),
-                ]),
-
-                html.Br(),
-                html.H4("Results"),
-                dbc.Row([dash_table.DataTable(
-                    id='sbc-tbl-results', data=[], columns=tbl_inter_company_header,
-                    style_cell={"whiteSpace": "pre-line"}, sort_action="native", sort_mode='multi',
-                    row_deletable=False, page_action='native', page_current=0, page_size=10
-                )])
-            ]),
-
-            # TAB 2
-            dcc.Tab(label='Entities', value='entities', children=[
-                html.H4("Search"),
-                dbc.Row([
-                    dbc.Col(dmc.TextInput(id="entities-filter-query", type="text", placeholder="Query", debounce=True), width=12, lg=4),
-                    dbc.Col(dmc.Select(id='entities-filter-country', data=[""] + target_countries, placeholder="Country", searchable=True), width=6, lg=3),
-                    dbc.Col(dmc.Select(id='entities-filter-schema', data=schemas, placeholder="Schema", searchable=True), width=6, lg=2),
-                    dbc.Col(dbc.Button(id='entities-btn-search', children="Search", color="light", className="me-1", n_clicks=0), width=6, lg=1),
-                    dbc.Col(dbc.Button(id='entities-btn-export', children="Excel Export", color="primary", className="me-1", n_clicks=0), width=6, lg=2)
-                ]),
-                html.Br(),
-                html.H4("Result"),
-                dbc.Row([dash_table.DataTable(
-                    id='entities-tbl-results', columns=entity_search_header, data=[],
-                    style_cell={"whiteSpace": "pre-line"}, sort_action="native", sort_mode='multi',
-                    row_deletable=False, page_action='native', page_current=0, page_size=10
-                )])
-            ]),
-
-            # TAB 3
-            dcc.Tab(label='Network Analysis', value='Network Analysis', children=[
-                html.H4("Filter"),
-                dbc.Row([
-                    dbc.Col(dmc.Select(id='network-filter-schema', data=schemas, placeholder="Schema", searchable=True), width=6, lg=2),
-                    dbc.Col(dmc.Select(id='network-filter-industry', data=industries, placeholder="Industries", searchable=True), width=6, lg=3),
-                    dbc.Col(dmc.DatePicker(id="network-filter-start-date", placeholder="Start Date", minDate=date(201, 5, 21)), width=6, lg=2),
-                    dbc.Col(dmc.DatePicker(id="network-filter-end-date", placeholder="End Date", minDate=date(201, 5, 21)), width=6, lg=2),
-                    dbc.Col(dbc.Button(id="network-btn-export", children="Excel Export", color="primary", className="me-1", n_clicks=0), width=4, lg=2),
-                ]),
-                html.Br(),
-                dbc.Row([
-                    dbc.Col(dmc.MultiSelect(id="network-filter-countries", searchable=True, placeholder="Countries", data=all_countries), width=12, lg=9),
-                    dbc.Col(dbc.Button(id="network-btn-load", children="Load", color="light", n_clicks=0), width=4, lg=1),
-                ]),
-                html.Br(),
-                html.H4("Who Sanctions Whom"),
-                dcc.Graph(id="network-graph"),
-
-                html.Br(),
-                html.H4("Centrality Scores"),
-                dash_table.DataTable(
-                    id='network-tbl-centralises',
-                    tooltip_header=tooltip_header, tooltip_delay=0, tooltip_duration=None,
-                    columns=network_metrics_header, data=[],
-                    style_cell={"whiteSpace": "pre-line"}, sort_action="native", sort_mode='multi',
-                    row_deletable=False, page_action='native', page_current=0, page_size=10)
+                    html.Br(),
+                    html.H4("Centrality Scores"),
+                    dash_table.DataTable(
+                        id='network-tbl-centralises',
+                        tooltip_header=tooltip_header, tooltip_delay=0, tooltip_duration=None,
+                        columns=network_metrics_header, data=[],
+                        style_cell={"whiteSpace": "pre-line"}, sort_action="native", sort_mode='multi',
+                        row_deletable=False, page_action='native', page_current=0, page_size=10)
+                ])
             ])
-        ])
-    ], fluid=True),
+        ], fluid=True),
 
-    html.Hr(),
+        html.Hr(),
 
-    dbc.Container([
+        dbc.Container([
 
-        html.Footer([
-            html.H3("Sanctions Dashboard"),
-            html.H5(["Find the Project on ", html.A("GitHub", href="https://github.com/HackerBschor/SanctionsDashboard")]),
-            html.H5("Disclaimer"),
-            html.P([
-                "This dashboard was created by TU Vienna student ",
-                html.A("Nicolas Bschor", href="https://github.com/HackerBschor"),
-                " in collaboration with WU Vienna Professor ",
-                html.A("Dr. Jakob Müllner", href="https://www.wu.ac.at/iib/iib/faculty/muellner/"),
-                " during the 'Interdisciplinary Project in Data Science' course.",
-                html.Br(),
-                "The data used in this dashboard is sourced from ",
-                html.A("OpenSanctions", href="https://www.opensanctions.org/"),
-                " (Sanctions Information) and ",
-                html.A("People Data Labs", href="https://www.peopledatalabs.com/"),
-                """ (Company Industries). We only applied various transformation techniques in order to allow the analysis
-                without changing the information. Therefore, we cannot ensure completeness, correctness, 
-                or if the data is up-to-date.
-                """,
-                html.Br(),
-                """
-                Users are advised to verify information independently, 
-                and the developers assume no responsibility for the consequences of its use. 
-                Use the dashboard at your own risk."""
+            html.Footer([
+                html.H3("Sanctions Dashboard"),
+                html.H5(["Find the Project on ", html.A("GitHub", href="https://github.com/HackerBschor/SanctionsDashboard")]),
+                html.H5("Disclaimer"),
+                html.P([
+                    "This dashboard was created by TU Vienna student ",
+                    html.A("Nicolas Bschor", href="https://github.com/HackerBschor"),
+                    " in collaboration with WU Vienna Professor ",
+                    html.A("Dr. Jakob Müllner", href="https://www.wu.ac.at/iib/iib/faculty/muellner/"),
+                    " during the 'Interdisciplinary Project in Data Science' course.",
+                    html.Br(),
+                    "The data used in this dashboard is sourced from ",
+                    html.A("OpenSanctions", href="https://www.opensanctions.org/"),
+                    " (Sanctions Information) and ",
+                    html.A("People Data Labs", href="https://www.peopledatalabs.com/"),
+                    """ (Company Industries). We only applied various transformation techniques in order to allow the analysis
+                    without changing the information. Therefore, we cannot ensure completeness, correctness, 
+                    or if the data is up-to-date.
+                    """,
+                    html.Br(),
+                    """
+                    Users are advised to verify information independently, 
+                    and the developers assume no responsibility for the consequences of its use. 
+                    Use the dashboard at your own risk."""
+                ])
             ])
-        ])
-    ], fluid=True),
+        ], fluid=True),
 
-    dcc.Download(id="sbc-download"),
-    dcc.Download(id="entities-download"),
-    dcc.Download(id="network-download"),
-])
+        dcc.Download(id="sbc-download"),
+        dcc.Download(id="entities-download"),
+        dcc.Download(id="network-download"),
+    ])
+
+    return app
 
 
 ###################################################################
@@ -306,4 +311,5 @@ def download_network(schema: str, industry: str, start_date: str, end_date: str,
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=False, host="0.0.0.0", port=3000)
